0f8183b29c7867d2409e77718c95e90e8b87eff7
IhorZolot <fedonn@ukr.net>
2024-06-14 12:45:25 +0300

    update first image

--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -3,19 +3,19 @@ import Image from 'next/image'
 
 import { VscArrowRight } from 'react-icons/vsc'
 
-import home1 from '@public/images/invite/home1.png'
-import image144 from '@public/images/invite/image144.png'
+import firstImg from '@public/images/firstPage/firstImg.png'
+import secondImg from '@public/images/firstPage/secondImg.png'
 import Button from '@/shared/Button/Button'
 
 const Invite = () => {
 	return (
 		<section className='mb-8 px-2 lg:mb-16 lg:px-8 '>
 			<Image
-				src={home1}
+				src={firstImg}
 				alt='Home'
 				className='w-full object-cover lg:hidden bg-gradient-to-r from-cyan-500 to-blue-500'
 			/>
-			<Image src={image144} alt='Home' className='hidden lg:block lg:w-full' />
+			<Image src={secondImg} alt='Home' className='hidden lg:block lg:w-full' />
 			<a
 				className='bg-mainBlue py-4 px-2 flex items-center hover:bg-yellow-500 hover:text-black transition-transform duration-900 text-white
 			 lg:hidden'
